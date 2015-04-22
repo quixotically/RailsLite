73ba048c796f07d6725d7435769090641f0b2895
Felicia <Felicia@appacademy.io>
2015-04-22 14:18:24 -0400

    Finish Rails Lite, all specs pass

--- lib/phase6/router.rb
+++ lib/phase6/router.rb
@@ -3,15 +3,30 @@ module Phase6
     attr_reader :pattern, :http_method, :controller_class, :action_name
 
     def initialize(pattern, http_method, controller_class, action_name)
+      @pattern = pattern
+      @http_method = http_method
+      @controller_class = controller_class
+      @action_name = action_name
     end
 
     # checks if pattern matches path and method matches request method
     def matches?(req)
+      @pattern === req.path &&
+      req.request_method.downcase.to_sym == @http_method
     end
 
     # use pattern to pull out route params (save for later?)
     # instantiate controller and call controller action
     def run(req, res)
+      matchdata = @pattern.match(req.path)
+      route_params = {}
+
+      matchdata.names.each do |name|
+        route_params[name] = matchdata[name]
+      end
+
+      controller = @controller_class.new(req, res, route_params)
+      controller.invoke_action(@action_name)
     end
   end
 
@@ -19,28 +34,37 @@ module Phase6
     attr_reader :routes
 
     def initialize
+      @routes = []
     end
 
     # simply adds a new route to the list of routes
     def add_route(pattern, method, controller_class, action_name)
+      @routes << Route.new(pattern, method, controller_class, action_name)
     end
 
     # evaluate the proc in the context of the instance
     # for syntactic sugar :)
     def draw(&proc)
+      self.instance_eval(&proc)
     end
 
     # make each of these methods that
     # when called add route
     [:get, :post, :put, :delete].each do |http_method|
+      define_method(http_method) do |pattern, controller_class, action_name|
+        add_route(pattern, http_method, controller_class, action_name)
+      end
     end
 
     # should return the route that matches this request
     def match(req)
+      @routes.find {|route| route.matches?(req)}
     end
 
     # either throw 404 or call run on a matched route
     def run(req, res)
+      route = match(req)
+      route ? route.run(req, res) : res.status = 404
     end
   end
 end
